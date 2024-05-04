0ce2a2fa8cb3c64b9a80250937eca4cd43c4603d
Alan Torres <158106033+ATC92@users.noreply.github.com>
2024-05-04 00:07:06 -0700

    Update Lenguage

--- Resources/funtions.py
+++ Resources/funtions.py
@@ -18,39 +18,41 @@ def inConsole(iteraciones, delay=0.01):
         # Le damos un delay, que ya esta pre colocado en el inicio de la def.
         time.sleep(delay)
 
-def YoutubeLink():
+def YoutubeLink(string):
     # Creamos una variable donde guardaremos el Link del video.
     # Verificamos que el link sea correcto
     while(True):
         try:
-            Link = input('Ingrese el enlace del video: ')
+            print(string["LinkVideo"],"",end="")
+            Link = input()
             YT_Object = YouTube(Link)
             return YT_Object
         except VideoUnavailable:
-            print(f"Video {Link}, no es válido, intenta de nuevo.")
+            print(string[f"ErrorVideoLoad"])
 
-def FormatSelection():
+def FormatSelection(string):
     try:
         # Preguntar al usuario que formato lo desea
-        print("En que formato lo deseas descargar: \n")
-        print("[1] - Video\n[2] - Audio")
+        print(string["OptionAudioVideo"])
+        print(string["OptionSelection"])
+        print(string["OptionSelection2"])
         format = int(input(">>> "))
         return format
     except ValueError:
-        print("Opcion Invalida, intenta de nuevo...\\n")
+        print(string["TryAgain"],"\n")
         
-def printInformation(YT_Object):
+def printInformation(YT_Object,string):
     # Muestra información básica del video
     # Dando Título y Autor del objeto.
-    print("Título: ", YT_Object.title, "\nAutor: ", YT_Object.author)
+    print(string["Tittle"], YT_Object.title, "\n",string["Autor"], YT_Object.author)
     print() 
     # Duración del video, donde calculamos la duración en segundos y minutos
     duration_seconds = int(YT_Object.length)
     minutes, seconds = divmod(duration_seconds, 60)
     # Muestra en consola la duración del video. Minutos y Segundos: MM:SS
-    print("Duración: ", "{}:{}".format(minutes, seconds), "\n")
+    print(string["Duration"], "{}:{}".format(minutes, seconds), "\n")
     
-def VideoConverter(format,YT_Object):
+def VideoConverter(format,YT_Object,string):
     try:
         # Verificamos lo que entra es elegido por el usuario.
         match format:
@@ -61,33 +63,67 @@ def VideoConverter(format,YT_Object):
                 # Busca las mejores opciones para descargar en el formato deseado y la resolucion deseada.
                 VideoOptions = YT_Object.streams.filter(only_audio=True) # , progressive=False, file_extension='mp4',audio_codec='mp4a.40.2').order_by('resolution').desc()
             case _:
-                print("Opcion erronea, intenta de nuevo...")
+                print(string["TryAgain"])
         return VideoOptions
     except Exception as e:
-        print("Error, inesperado",e)
+        print(string["OcurrioError"],e)
 
-def VideoResolution(VideoOptions):
+def VideoResolution(VideoOptions,string):
     # Obtenemos la direccion del Escritorio
     desktopDirectory= os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
     # Despliega las opciones disponibles en resolución.
-    print("Opciones de calidad disponibles: ")
+    print(string["Quality"])
     for i, Video in enumerate(VideoOptions):
-        print(f"{i + 1}. {Video.resolution} - {Video.mime_type} - {Video.filesize / (1024*1024):.2f} MB")
+        print(f"[{i + 1}]. {Video.resolution} - {Video.mime_type} - {Video.filesize / (1024*1024):.2f} MB")
     # Solicita al usuario que seleccione la calidad deseada
     # Ciclamos la opción para que el usuario ingrese una opción correcta.
     while True:
-        selection = input("Ingrese la opción deseada (Recuerda usar número): ")
+        print(string["OptionNumber"],"",end="")
+        selection = input()
         try:
             option = int(selection)
             break
         except ValueError:
-            print("Intenta de nuevo.\n\n")
+            print(string["Error"],"\n\n")
     # Verifica si la opción seleccionada es válida
     if 1<= option <= len(VideoOptions):  
         # Obtiene la información del video seleccionado.
         VideoSelected = VideoOptions[option-1]
         # Descarga el video en la calidad deseada.
         VideoSelected.download(output_path=desktopDirectory) 
-        print("Descarga completa.\n\n") 
+        print(string["Download"],"\n\n") 
     else:
-        print("Selección de calidad inválida")  # Indica que la opción seleccionada no es válida
+        print(string["SelectionInvalid"])  # Indica que la opción seleccionada no es válida
+        
+def cargar_strings(idioma):
+    # Diccionario para almacenar las cadenas de texto
+    strings = {}
+
+    # Determinar la ruta del archivo según el idioma seleccionado
+    ruta_archivo = f"Resources/Language/strings_{idioma}.txt"
+
+    # Leer las cadenas de texto desde el archivo
+    with open(ruta_archivo, "r", encoding="utf-8") as file:
+        for linea in file:
+            # Separar la línea en clave y valor
+            clave, valor = linea.strip().split(" = ")
+            # Agregar la clave y valor al diccionario
+            strings[clave] = valor
+
+    return strings
+
+def SelectionLenguaje():
+    while True:
+        try:
+            print("[1] - Español\n[2] - English")
+            option = int(input(">>>> "))
+        except ValueError:
+            print("Error")
+        if(option > 0 and option < 3):
+            if(option == 1):
+                option = "es"
+            elif (option == 2):
+                option = "en"
+            break
+        
+    return option
--- main.py
+++ main.py
@@ -4,34 +4,42 @@ from Resources.funtions import *
 from os import system
 
 def main():
+    # Barra de inicio
     inConsole(30)
     print("\n")
+    # Seleccionamos el idioma
+    option = SelectionLenguaje()
+    # Cargamos la opcion
+    string = cargar_strings(option)
     # Try para verificar algún error.
     try:
         while(True):
             # Lo mandamos a objeto de YouTube. (Formato video)      
-            YT_Object = YoutubeLink()
+            YT_Object = YoutubeLink(string)
             # Imprimimos la información
-            printInformation(YT_Object)
+            printInformation(YT_Object,string)
             # Elección para el formato.
-            format = FormatSelection()
+            format = FormatSelection(string)
             # Damos formato al objeto para comenzar la descarga.
-            available_streams = VideoConverter(format,YT_Object)
+            available_streams = VideoConverter(format,YT_Object,string)
             # Selección de la resolución
-            VideoResolution(available_streams)
+            VideoResolution(available_streams,string)
             # Pregunta al usuario para repetir el programa.
             while(True):
                 try:
-                    Question = int(input("Desea ingresar otro link: [1] - SI . [0] - NO: "))
+                    print(string["RepetirPrograma"])
+                    print(string["RepetirPrograma2"])
+                    Question = int(input(">>>>> "))
                     break
                 except ValueError:
-                    print("Error, intenta de nuevo.\n")
+                    print(string["Error"],"\n")
             if Question == 0:
                 break
             system('cls')
     # En caso de algún error inesperado, lanzará un error.
     except Exception as e:
-        print('Ocurrio un error:', e) 
+        print(string["OcurrioError"], e) 
+        
 # Inicio del programa.
 if __name__ == "__main__":
     main()
